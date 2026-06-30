eb78e80725b4867ec22b77c58066cfa8696eea08
opdkktu <opd.kktu@moh.gov.my>
2026-06-30 10:00:31 +0800

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  GAS_URL: "https://script.google.com/macros/s/AKfycbxvdfTsoFS8TX2cPNLqnXAwqCpnO7tjVDNClNWc8IBQ6so3Y8wqHYOL3opM7cms6zplnQ/exec",
+  SHARED_TOKEN: "JKNKEDAH"
+};
