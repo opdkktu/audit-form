c58d95b03bf0cdde9dd5bbe4c457aa865795a447
opdkktu <opd.kktu@moh.gov.my>
2026-07-07 15:28:15 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  FORM1_GAS_URL:  "https://script.google.com/macros/s/AKfycbxvdfTsoFS8TX2cPNLqnXAwqCpnO7tjVDNClNWc8IBQ6so3Y8wqHYOL3opM7cms6zplnQ/exec",
+  GAS_URL:  "https://script.google.com/macros/s/AKfycbxvdfTsoFS8TX2cPNLqnXAwqCpnO7tjVDNClNWc8IBQ6so3Y8wqHYOL3opM7cms6zplnQ/exec",
   FORM2_GAS_URL: "https://script.google.com/macros/s/AKfycbzSa6SG4JrH7VlvIj0Nck1D9-fvMdITIPDQC--31TeYGXuKm0ty73utqBDgoY5NriFj/exec",
   SHARED_TOKEN: "JKNKEDAH"
 };
