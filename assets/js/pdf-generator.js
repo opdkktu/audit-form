8cab311813980be30500550b00f8c901f4d3a85b
opdkktu <opd.kktu@moh.gov.my>
2026-07-08 15:04:26 +0800

    Add 'Tempat Bertugas' to PDF info section

--- assets/js/pdf-generator.js
+++ assets/js/pdf-generator.js
@@ -45,7 +45,7 @@ async function generateAuditPdf({ header, answers, ringkasan, ambulansStatus, am
   let y = 88;
   const info = [
     ["Daerah", header.daerah], ["Klinik", header.klinik],
-    ["Juruaudit", header.namaPPP], ["Tarikh Audit", header.tarikhAudit]
+    ["Juruaudit", header.namaPPP],  ["Tempat Bertugas", header.tempatBertugas || "-"],["Tarikh Audit", header.tarikhAudit]
   ];
   doc.setFontSize(9.5);
   info.forEach(([label, val]) => {
